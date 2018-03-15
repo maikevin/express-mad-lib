b1192130bf464cacb663fd1b088aadf678744db1
Glitch (sparkling-xylophone) <none>
2018-03-15 04:22:50 +0000

    🌊💧 Checkpoint ./server.js:245842/456

--- server.js
+++ server.js
@@ -6,14 +6,14 @@
 const express = require('express');
 const app = express();
 
-// use express middleware to put key-value pairs in the request body into req.body
-// you'll learn all about middleware in the next assignment!
+// use express middleware to parse the request body and add it to the request object
+// don't worry, you'll learn all about middleware in the next assignment!
 app.use(express.json());
 
 // your code here.
 
-// hint: in Postman under Body remember to select JSON instead of Text, and try logging
-// req.body to the console to find the key-value pairs submitted from Postman, and 
+// hint: in Postman under Body remember to select JSON instead of Text, then try doing
+// console.log(req.body) here to show your key-value pairs from Postman in the Logs
 
 
 // listen for requests :)
